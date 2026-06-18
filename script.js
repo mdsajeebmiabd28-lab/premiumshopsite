ace30d6b39ecc26235aa0a1adee0e024a8b35036
mdsajeebmiabd28-lab <mdsajeebmiabd28@gmail.com>
2026-06-18 23:27:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ products.forEach(product => {
     const card = document.createElement('div');
     card.className = 'product-card';
     card.innerHTML = `
+        <div class="product-icon">
+            <i class="${product.icon || 'fa-solid fa-vault'}"></i>
+        </div>
         <h3>${product.name}</h3>
         <p class="price">${product.price}</p>
-        <a href="${product.telegramLink}" target="_blank" class="buy-btn">Buy via Telegram</a>
+        <p class="validity">Valid: ${product.valid}</p>
+        <a href="${product.telegramLink}" target="_blank" class="buy-btn">
+            <i class="fa-brands fa-telegram"></i> Buy Now
+        </a>
     `;
     container.appendChild(card);
 });
