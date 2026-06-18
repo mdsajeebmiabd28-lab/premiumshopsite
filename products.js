3753d3ac992b7b0592947b35f1c84ad34f7818f7
mdsajeebmiabd28-lab <mdsajeebmiabd28@gmail.com>
2026-06-18 22:47:24 +0600

    Create products.js

---
+++ products.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        id: 1,
+        name: "Premium VPN - 1 Month",
+        price: "$5.00", 
+        telegramLink: "https://t.me/your_username" 
+    },
+    {
+        id: 2,
+        name: "Residential IP Proxy",
+        price: "$10.00", 
+        telegramLink: "https://t.me/your_username" 
+    }
+];
